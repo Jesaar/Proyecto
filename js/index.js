15be714c1ff966685dcf2046e12adfbe4207ab77
Jesaar <jesaar2000@gmail.com>
2021-09-18 10:42:22 -0500

    Proyecto nuevo

---
+++ js/index.js
@@ -0,0 +1,8 @@
+gsap.from('.slider',{
+    duration: 4,
+    x: 400,
+    delay: 1,
+    borderRadius: '60%',
+    ease:'bounce.out', 
+});
+
---
+++ js/productos.js
@@ -0,0 +1,17 @@
+var sound = new Howl ({
+    src=['.Proyecto/js/WhatsApp.mp3']
+});
+
+var btnPlay = document.getElementById("btnPlay");
+btnPlay.addEventListener("click", function(){
+    sound.play()
+    btnPlay.className="inhabilitar"
+    btnPause.className="habilitar"
+});
+
+var btnPause = document.getElementById("btnPause");
+btnPause.addEventListener("click", function(){
+    sound.pause()
+    btnPlay.className="habilitar"
+    btnPause.className="inhabilitar"
+});
